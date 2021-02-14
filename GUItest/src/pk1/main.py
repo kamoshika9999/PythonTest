a4b83419231aaf0a94f49381150e9f38f74e2aca
kamoshika9999 <hosokawamamoru@oboe.ocn.ne.jp>
2021-02-14 11:30:56 +0900

    test

---
+++ GUItest/src/pk1/main.py
@@ -0,0 +1,30 @@
+'''
+Created on 2021/02/13
+
+@author: Mamoru
+'''
+import sys
+from PyQt5.QtCore import pyqtSlot
+from  PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.uic import loadUi
+from src.pk1.ui_test import Ui_MainWindow
+
+
+class GUItest(QMainWindow,Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(GUItest,self).__init__(parent)
+        self.setupUi(self)
+        #loadUi('test.ui',self)
+        #self.pushButton.clicked.connect(self.onPushButtonCliked)
+
+    #@pyqtSlot()
+    #def onPushButtonCliked(self):
+     #   self.label1.setText(self.textEdit.text)
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    widget=GUItest()
+    widget.show()
+    sys.exit(app.exec_())
+
+
